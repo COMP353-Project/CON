4c4fb0d927981b30fb526ae4dd2bc330d549415f
Kenza <kenza.boulisfane@gmail.com>
2020-12-14 14:48:40 -0500

    Fix group deletion

--- api/groups/deleteGroup.php
+++ api/groups/deleteGroup.php
@@ -12,11 +12,11 @@ $table_name = 'Groups';
 if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
   $name = $_DELETE['name'];
   
-  $query = "DELETE FROM  " . $table_name . " WHERE name = '$name'";
+  $query = "DELETE FROM `Groups` WHERE name = '$name'";
   mysqli_query($conn, $query);
   $successful = mysqli_affected_rows($conn);
 
-  if ($successful) {
+  if ($successful == 1) {
       http_response_code(200);
       echo json_encode(array("message" => "Group was successfully deleted."));
   } else {
