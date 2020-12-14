8500ddd26d10da39647e0f8bb2179c17ff67b5af
KonstH <konstantinhristev@gmail.com>
2020-12-14 13:05:30 -0500

    Adds ability for admins to create public posts

--- src/Components/AdminDashboard/AdminPost.js
+++ src/Components/AdminDashboard/AdminPost.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import Nav from './AdminNav';
+import Spinner from '../Global/Spinner';
 import './AdminStyles.css';
 import TextField from '@material-ui/core/TextField';
+import { Context as AdminContext } from '../../context/AdminContext';
 
 function AdminPost () {
+  const { createAdminPost, state: { error, success, isLoading } } = React.useContext(AdminContext);
   const [title, setTitle] = React.useState("");
   const [content, setContent] = React.useState("");
+
+  const handlePost = (e) => {
+    e.preventDefault();
+
+    const info = {
+      id: localStorage.getItem('userid'),
+      title: title,
+      content: content
+    }
+
+    createAdminPost(info);
+
+    // Reset form
+    setTitle('');
+    setContent('');
+  }
 
   return (
     <div>
       <Nav />
       <div className="container--admin">
-        <form className="container--form">
+        <form className="container--form" onSubmit={handlePost}>
           <h3 className="form__title">Create Post</h3>
           <div className="form__field">
             <TextField
@@ -33,16 +52,15 @@ function AdminPost () {
               value={content}
               required
               multiline
+              rows="5"
               onChange={e => setContent(e.target.value)}
             />          
           </div>
-          <div className="form__field">
-            <label>Attachments</label> 
-            <input type="file" id="admin-post-attachments" accept=".jpg,.png,.gif"></input>
-          </div>
           <div className="btn-container">
-            <button className="post-btn" /**onClick={handleClick}**/>POST</button>
+            {isLoading ? <Spinner /> : <input type="submit" value="POST" className="post-btn" />}
           </div>
+          {error === "createAdminPost" && <p className="is-error primary">Couldn't create post</p>}
+          {success === "createAdminPost" && <p className="is-success">Post created</p>}
         </form>
       </div>
 
