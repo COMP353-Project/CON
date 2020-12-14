559483ad5735c5ac675838832580a87d3be4506f
Kenza <kenza.boulisfane@gmail.com>
2020-12-14 04:20:41 -0500

    Implement 'My Groups' - Fetch groups the logged in user is part of - Fetch information about a specific group by id - Add Autocomplete in condo association deletion - Fetch group members for each specific group

--- api/groups/fetch.php
+++ api/groups/fetch.php
@@ -4,23 +4,23 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
 
-$rest_json = file_get_contents("php://input");
 $conn = mysqli_connect("localhost", "root", "", "con");
-$user_id = $_GET['id'];
 
-$query = "SELECT id, name, description FROM `Groups` INNER JOIN Group_members WHERE Groups.id = Group_members.group_id AND Group_members.user_id = '$user_id'";
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+$query = "SELECT name FROM `Groups`";
 
-$result = mysqli_query($conn, $query);
+$result = @mysqli_query($conn, $query);
 
 if ($result->num_rows > 0) {
-    $myGroups = array();
+    $array = array();
     while($row = $result->fetch_assoc()) {
-        array_push($myGroups, $row);
+        array_push($array, $row);
   }
-    echo json_encode($myGroups);
+    echo json_encode($array);
     http_response_code(200);
 } 
  else {
     http_response_code(400);
     echo json_encode(array("message" => "Unable to fetch groups"));
+}
 }
--- src/Components/Groups/MyGroups.js
+++ src/Components/Groups/MyGroups.js
@@ -1,61 +1,57 @@
-import { Button, ListItem, ListItemText, List, Container } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../css/GroupsStyle.css'
 import CreateGroupButton from './CreateGroupBtn';
 import GroupsNav from './GroupsNav.js'
-import {Context as GroupsContext} from '../../context/GroupsContext.js'
+import { Context as GroupsContext } from '../../context/GroupsContext.js'
 
 function MyGroups () {
-    const { fetchMyGroups, fetchGroup } = React.useContext(GroupsContext);
-    const [groups, setGroups] = React.useState([])
+  const { fetchMyGroups } = React.useContext(GroupsContext);
+  const [groups, setGroups] = React.useState([]);
 
-    const isAdmin = true;
-    const onDelete = () => {
+  const getMyGroups = async () => {
+    setGroups(await fetchMyGroups(localStorage.getItem("userid")));
+  }
 
+  React.useEffect(() => {
+    getMyGroups();
+  }, []);
+
+  const RenderGroups = () => {
+    if (groups) {
+      return groups.map(({ id, name, description }) => {
+        return (
+          <React.Fragment key={id}>
+            <div className="card">
+              <div className='card-title'>{name}</div>
+              <div className='card-description'>{description}</div>
+              {< Link to={`/groups/${id}/home`}>
+                <div className="details-button">View Details</div>
+              </Link>}
+            </div>
+          </React.Fragment>
+        );
+      });
     }
-
-    const getMyGroups = async() => {
-      setGroups( await fetchMyGroups(localStorage.getItem("userid")));
+    else {
+      return (
+        <div className="friend-empty">No groups!</div>
+      );
     }
+  };
 
-    React.useEffect(() => {
-      getMyGroups();
-    }, []);
-
-    const sendIDtoLocalStorage = (groupID) => {
-        localStorage.setItem("groupID", groupID)
-        console.log(localStorage.getItem("groupID"))
-    }
-
-    return (
-      <div>
-        <GroupsNav/>
-        <div className="page-container">
-          <div className="page-header">
-            <h1>My Groups</h1>
-            <CreateGroupButton />
-          </div>
-          <div className="groups-container">
-              { groups.map(group => {
-                return (
-                  <div className="group-container">
-                      <Button onClick={() => sendIDtoLocalStorage(group.id)}component={Link} 
-                          to={{
-                              pathname: "/groups/" + group.id +"/home",
-                              state: { id: group.id}
-                          }}>
-                          <p>{"Click this to go to group " + group.id}</p>
-                      </Button>
-                      <div>Users go here</div>
-                      {isAdmin ? <Button>Delete</Button> : <div></div>}
-                  </div>
-                );
-              })}
-          </div>
+  return (
+    <div>
+      <GroupsNav />
+      <div className="page-container">
+        <div className="page-header">
+          <h1>My Groups</h1>
+          <CreateGroupButton />
         </div>
+        <RenderGroups />
       </div>
-    );
+    </div>
+  );
 }
 
 export default MyGroups;
